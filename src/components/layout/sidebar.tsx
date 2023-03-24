5f09b2ba11fbd7178b07403c1ba079e117ee2017
SukkaW <isukkaw@gmail.com>
2023-03-24 20:34:07 +0800

    Fix sidebar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,7 +1,7 @@
 import { memo } from 'react';
 import { Navbar, NavLink as MantineNavLink, rem, createStyles, Text, Group, Button } from '@mantine/core';
 import { Link, NavLink as ReactRouterNavLink, useParams } from 'react-router-dom';
-import type { Icon} from '@tabler/icons-react';
+import type { Icon } from '@tabler/icons-react';
 import { IconArrowLeft } from '@tabler/icons-react';
 
 import { navLinks } from '@/router';
@@ -49,15 +49,19 @@ function SidebarContent() {
 
   return (
     <>
-      <Navbar.Section p="md" sx={theme => ({ borderBottom: `${rem(1)} solid ${theme.colors.gray[2]}` })}>
-        <Group spacing="sm">
-          <Button component={Link} to="/" variant="subtle" p={8}>
-            <IconArrowLeft size={rem(24)} />
-          </Button>
+      {
+        zoneName && (
+          <Navbar.Section p="md" sx={theme => ({ borderBottom: `${rem(1)} solid ${theme.colors.gray[2]}` })}>
+            <Group spacing="sm">
+              <Button component={Link} to="/" variant="subtle" p={8}>
+                <IconArrowLeft size={rem(24)} />
+              </Button>
 
-          <Text fw={600} size="lg">{zoneName}</Text>
-        </Group>
-      </Navbar.Section>
+              <Text fw={600} size="lg">{zoneName}</Text>
+            </Group>
+          </Navbar.Section>
+        )
+      }
       <Navbar.Section p="md" grow>
         {navLinks.map((link) => (
           <NavLink
